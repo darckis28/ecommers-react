ee562dfdf46f5ba436dbea455e6641fe88879281
andi aguirre <darckisgamer@gmail.com>
2025-01-08 11:50:33 -0500

    add: update ButtonBlack component to center content and add gap between elements

--- src/components/ButtonBlack.jsx
+++ src/components/ButtonBlack.jsx
@@ -1,6 +1,6 @@
 const ButonBlack = ({ children }) => {
   return (
-    <button className="bg-black w-full rounded-3xl py-3 text-white text-normal lg:w-[210px]">
+    <button className="bg-black w-full rounded-3xl py-3 text-white text-normal lg:w-[210px] flex items-center justify-center gap-2">
       {children}
     </button>
   );
