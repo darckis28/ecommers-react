f0a9521948a42f0bd4c95d20735dd2b59e21459f
andi aguirre <darckisgamer@gmail.com>
2025-01-08 21:16:50 -0500

    add: integrar react-hook-form y mejorar el componente Header con opciones de autenticación

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,43 +4,65 @@ import HamburgerIcon from "../icons/Hamburger";
 import Search from "../icons/Search";
 import User from "../icons/User";
 import Name from "./Name";
+import { useState } from "react";
+import { useContextCommers } from "../hooks/useContextCommers";
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+  const { token } = useContextCommers();
+
   return (
     <header className="flex justify-between items-center p-4">
       <div className="flex gap-2 items-center">
-        <span>
+        {/* <span>
           <HamburgerIcon />
-        </span>
+        </span> */}
         <Name />
       </div>
-      {/* <nav>
-        <ul className="flex gap-4">
-          <li>
-            <a
-              href="#"
-              className="flex gap-1 items-center"
-            >
-              Shop <Down />
-            </a>
-          </li>
-          <li>
-            <a href="#">On Sale</a>
-          </li>
-          <li>
-            <a href="#">New Arrivals</a>
-          </li>
-          <li>
-            <a href="#">Brands</a>
-          </li>
-        </ul>
-      </nav> */}
       <div className="flex gap-2">
-        <span>
+        {/* <span>
           <Search />
-        </span>
-        <span>
+        </span> */}
+        <span
+          onClick={() => setShow(!show)}
+          className="block relative cursor-pointer"
+        >
           <User />
+          {show && (
+            <div className="bg-[#f0f0f0] p-4 absolute bottom-0 right-2 translate-y-full  z-50">
+              <ul className="text-black/60 ">
+                {!token ? (
+                  <>
+                    <li>
+                      <Link
+                        className="hover:text-red-400"
+                        to={"/login"}
+                      >
+                        Login
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className="hover:text-red-400"
+                        to={"/register"}
+                      >
+                        Register
+                      </Link>
+                    </li>
+                  </>
+                ) : (
+                  <li>
+                    <p
+                      className="hover:text-red-400"
+                      to={"/login"}
+                    >
+                      Logout
+                    </p>
+                  </li>
+                )}
+              </ul>
+            </div>
+          )}
         </span>
         <Link to={"/cart"}>
           <Cart />
